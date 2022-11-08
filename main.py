0d28538adf66715553b1004f494a508c73a7f781
njizza <njizzard@gmail.com>
2022-11-08 23:22:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X_full = pd.read_csv('./inputs/train.csv', index_col='Id')
+X_test_full = pd.read_csv('./inputs/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X_full.SalePrice
+X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+# To keep things simple, only use numerical predictors
+X = X_full.select_dtypes(exclude=['object'])
+X_test = X_test_full.select_dtypes(exclude=['object'])
+
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+
+print(X_train.head())
