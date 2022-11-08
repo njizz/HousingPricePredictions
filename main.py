8c406cbe222bdbf997220a04f6e2bd5e643557cf
njizza <njizzard@gmail.com>
2022-11-08 23:45:51 +0000

    Generated test predictions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.impute import SimpleImputer
 
 # Read the data
 X_full = pd.read_csv('./inputs/train.csv', index_col='Id')
@@ -20,3 +23,66 @@ X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test
                                                       random_state=0)
 
 print(X_train.head())
+
+# Shape of training data (num_rows, num_columns)
+print(X_train.shape)
+
+# Number of missing values in each column of training data
+missing_val_count_by_column = (X_train.isnull().sum())
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
+
+
+# Function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+
+# Get names of columns with missing values
+columns_with_missing_values = X_train.columns[X_train.isnull().any()].tolist()
+
+# Drop columns in training and validation data
+reduced_X_train = X_train.drop(columns_with_missing_values, axis=1)
+reduced_X_valid = X_valid.drop(columns_with_missing_values, axis=1)
+
+print("\nMAE (Drop columns with missing values):")
+print(score_dataset(reduced_X_train, reduced_X_valid, y_train, y_valid))
+
+# Imputation
+my_imputer = SimpleImputer() # Your code here
+imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+
+# Imputation removed column names; put them back
+imputed_X_train.columns = imputed_X_train.columns = X_train.columns
+imputed_X_valid.columns = X_valid.columns
+
+print("\nMAE (Imputation):")
+print(score_dataset(imputed_X_train, imputed_X_valid, y_train, y_valid))
+
+# Imputation 'median' strategy
+final_imputer = SimpleImputer(strategy='median')
+final_X_train = pd.DataFrame(final_imputer.fit_transform(X_train))
+final_X_valid = pd.DataFrame(final_imputer.transform(X_valid))
+
+# Imputation removed column names; put them back
+final_X_train.columns = X_train.columns
+final_X_valid.columns = X_valid.columns
+
+# Define and fit model
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+model.fit(final_X_train, y_train)
+
+# Get validation predictions and MAE
+preds_valid = model.predict(final_X_valid)
+print("\nMAE (Imputation 'median' strategy):")
+print(mean_absolute_error(y_valid, preds_valid))
+
+# Preprocess test data
+final_X_test = pd.DataFrame(final_imputer.transform(X_test))
+
+# Get test predictions
+preds_test = model.predict(final_X_test)
+
