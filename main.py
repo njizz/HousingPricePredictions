280b478c9b55ecd265293448c3ec95971a7e080c
njizza <njizzard@gmail.com>
2022-11-10 12:18:32 +0000

    Added plots

--- main.py
+++ main.py
@@ -1,50 +1,60 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.impute import SimpleImputer
 from typing import Tuple
 
-# Read the data
-X_full = pd.read_csv('./inputs/train.csv', index_col='Id')
-X_test_full = pd.read_csv('./inputs/test.csv', index_col='Id')
-
-# Remove rows with missing target, separate target from predictors
-X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
-y = X_full.SalePrice
-X_full.drop(['SalePrice'], axis=1, inplace=True)
-
-# Only use numerical predictors
-X = X_full.select_dtypes(exclude=['object'])
-X_test = X_test_full.select_dtypes(exclude=['object'])
-
-# Break off validation set from training data
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
-                                                      random_state=0)
-
-# Check out the data
-print(X_train.head())
-print(X_train.shape)
-missing_val_count_by_column = (X_train.isnull().sum())
-print(missing_val_count_by_column[missing_val_count_by_column > 0])
-
 # List for capturing MAE scores
 scores = []
 
 
-def evaluate_dataset(X_train, X_valid, y_train, y_valid) -> Tuple[np.ndarray, float]:
-    """Returns the model predictions and determines the mean absolute error of the model predictions against
-       the actual observations"""
-    model = RandomForestRegressor(n_estimators=100, random_state=0)
-    model.fit(X_train, y_train)
-    preds = model.predict(X_valid)
-    mae = mean_absolute_error(y_valid, preds)
-
-    return preds, mae
+def read_data(path: str) -> pd.DataFrame:
+    """Reads data from csv and returns a DataFrame"""
+    return pd.read_csv(path, index_col='Id')
 
 
-def drop_incomplete_columns(X_train, X_valid) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def get_training_data() -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """Remove rows with missing target, separates target from predictors, only uses numerical predictors, breaks off
+        validation set from training set and returns, test, training and validations datasets"""
+    X_full = read_data('./inputs/train.csv')
+    X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+    y = X_full.SalePrice
+    print(type(y))
+    X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+    X = X_full.select_dtypes(exclude=['object'])
+
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                          random_state=0)
+
+    return X_train, X_valid, y_train, y_valid
+
+
+def get_test_data() -> pd.DataFrame:
+    """Remove rows with missing target, separates target from predictors, only uses numerical predictors, breaks off
+        validation set from training set and returns, test, training and validations datasets"""
+    X_test_full = read_data('./inputs/test.csv')
+    X_test = X_test_full.select_dtypes(exclude=['object'])
+
+    return X_test
+
+
+def plot_correlations(X: pd.DataFrame, y: pd.Series) -> None:
+    """Plots the correlation of features with target"""
+    for column in X.columns:
+        plt.figure()
+        plt.scatter(X[column], y)
+        plt.title(f"{column} v Sales Price", fontsize=18)
+        plt.xlabel(column, fontsize=14)
+        plt.ylabel("Sales Price", fontsize=14)
+        plt.subplots_adjust(left=0.15)
+        plt.show()
+
+
+def drop_incomplete_columns(X_train: pd.DataFrame, X_valid: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Drops columns with missing values"""
     columns_with_missing_values = X_train.columns[X_train.isnull().any()].tolist()
 
@@ -55,7 +65,7 @@ def drop_incomplete_columns(X_train, X_valid) -> Tuple[pd.DataFrame, pd.DataFram
     return reduced_X_train, reduced_X_valid
 
 
-def imputation(X_train, X_valid, strategy: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def imputation(X_train: pd.DataFrame, X_valid: pd.DataFrame, strategy: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Assigns values to missing data based on simple averaging strategies"""
     imputer = SimpleImputer(strategy=strategy)
     imputed_X_train = pd.DataFrame(imputer.fit_transform(X_train))
@@ -68,7 +78,20 @@ def imputation(X_train, X_valid, strategy: str) -> Tuple[pd.DataFrame, pd.DataFr
     return imputed_X_train, imputed_X_valid
 
 
-def get_mae(strat_X_train, strat_X_valid, y_train, y_valid, strategy: str) -> str:
+def evaluate_dataset(X_train: pd.DataFrame, X_valid: pd.DataFrame, y_train: pd.Series, y_valid: pd.Series) \
+                     -> Tuple[np.ndarray, float]:
+    """Returns the model predictions and determines the mean absolute error of the model predictions against
+       the actual observations"""
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    mae = mean_absolute_error(y_valid, preds)
+
+    return preds, mae
+
+
+def get_mae(strat_X_train: pd.DataFrame, strat_X_valid: pd.DataFrame, y_train: pd.Series, y_valid: pd.Series,
+            strategy: str) -> str:
     """Calculates MAE and adds it to list of MAE scores"""
     global scores
     strat_preds, strat_score = evaluate_dataset(strat_X_train, strat_X_valid, y_train, y_valid)
@@ -76,6 +99,17 @@ def get_mae(strat_X_train, strat_X_valid, y_train, y_valid, strategy: str) -> st
     message = f"\nMAE ({strategy}): {strat_score}"
     return message
 
+
+# ----- GET DATA AND CHECK IT OUT -----
+X_train, X_valid, y_train, y_valid = get_training_data()
+X_test = get_test_data()
+
+print(X_train.head())
+print(X_train.shape)
+missing_val_count_by_column = (X_train.isnull().sum())
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
+plot_correlations(X_train, y_train)
+# -------------------------------------
 
 # ----- STRATEGY 1: Drop columns with missing data -----
 strat1_X_train, strat1_X_valid = drop_incomplete_columns(X_train, X_valid)
@@ -97,7 +131,7 @@ print(f"\nStrategy {scores.index(min(scores)) + 1} produces the lowest MAE")
 
 # Preprocess test data and get test predictions
 final_X_train, final_X_test = imputation(X_train, X_test, 'median')
-model = RandomForestRegressor(n_estimators=100, random_state=0)
-model.fit(final_X_train, y_train)
-test_preds = model.predict(final_X_test)
+chosen_model = RandomForestRegressor(n_estimators=100, random_state=0)
+chosen_model.fit(final_X_train, y_train)
+test_preds = chosen_model.predict(final_X_test)
 
